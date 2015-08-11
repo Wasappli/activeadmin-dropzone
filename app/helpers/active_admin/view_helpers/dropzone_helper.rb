7dc08273e2cf1cc397fdcda725a0ddb023637b16
Jérémy Lagrue <jeremy@wasapp.li>
2015-08-11 14:47:19 -0400

    working copy ?

--- app/helpers/active_admin/view_helpers/dropzone_helper.rb
+++ app/helpers/active_admin/view_helpers/dropzone_helper.rb
@@ -2,13 +2,17 @@ module ActiveAdmin::ViewHelpers::DropzoneHelper
 
   def render_mock_dropzone_files(dropzone_objects)
     dropzone_objects.map do |dropzone_object|
-      {
-        id: dropzone_object.id,
-        name: dropzone_object.send(dropzone_object.class.dropzone_field(:title)).squish,
-        size: dropzone_object.send(dropzone_object.class.dropzone_field(:file_size)),
+      { 
+        id: dropzone_object.id, 
+        name: dropzone_object.send(dropzone_object.class.dropzone_field(:title)),
+        size: dropzone_object.send(dropzone_object.class.dropzone_field(:file_size)), 
         url: dropzone_object.send(dropzone_object.class.dropzone_field(:url))
       }
-    end.to_json.html_safe
+    end.map do |hash|
+      hash.map{ |(k, v)| "#{ k }: '#{ v }'" }.join(',')
+    end.map do |string|
+      "{#{ string }}"
+    end.join(', ').html_safe
   end
-
+  
 end
